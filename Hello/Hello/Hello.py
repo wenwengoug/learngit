dc0c411a6ad96f7d694bdaead2408e4962af3af9
wenwengoug <wenda32@126.com>
2018-03-05 13:46:00 +0800

    增加斐波那契方法

--- Hello/Hello/Hello.py
+++ Hello/Hello/Hello.py
@@ -1,6 +1,60 @@
 
-print("Hello,python!")
-print("Modify by GitHub!")
-print("Modify by GitHub!")
-print("Modify by GitHub!")
-print("Modify by GitHub!")
+def fab(max):
+    n,a,b=0,0,1
+    list = []
+    while n<max:
+        list.append(b)
+        a,b=b,a+b
+        n=n+1
+    return list
+
+class Fab(object):
+    def __init__(self, max): 
+        self.max = max 
+        self.n, self.a, self.b = 0, 0, 1 
+    def __iter__(self): 
+        return self 
+    def next(self): 
+        if self.n < self.max: 
+            r = self.b 
+            self.a, self.b = self.b, self.a + self.b 
+            self.n = self.n + 1 
+            return r 
+        raise StopIteration()
+
+def fabyield(max): 
+    n, a, b = 0, 0, 1 
+    while n < max: 
+        yield b 
+        # print b 
+        a, b = b, a + b 
+        n = n + 1 
+
+def read_file(fpath): 
+    BLOCK_SIZE = 1024*10 
+    with open(fpath, 'rb') as f: 
+        while True: 
+            block = f.read(BLOCK_SIZE) 
+            if block: 
+                yield block 
+            else: 
+                return
+
+
+list = []
+list = fab(10)
+n = Fab(10)
+print(n.next())
+print(n.next())
+print(n.next())
+print(n.next())
+print(n.next())
+print(n.next())
+print(n.next())
+print(n.next())
+print(n.next())
+for n in fabyield(10):
+    print(n)
+
+for n in read_file("D:\\1.txt"):
+    print(n)
